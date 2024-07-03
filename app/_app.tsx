d996bb4c7d5720e90bdb8c06c2e479e0f6260d8a
sebastiantare <sebastiantarebustos@gmail.com>
2024-07-03 01:36:30 -0400

    fix type

--- app/_app.tsx
+++ app/_app.tsx
@@ -1,12 +1,21 @@
-import { SessionProvider } from "next-auth/react"
+import { AppProps } from 'next/app';
+import { NextComponentType } from 'next';
+import { SessionProvider } from 'next-auth/react';
+import { Session } from 'next-auth';
 
-export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
-}) {
+interface MyAppProps extends AppProps {
+  Component: NextComponentType;
+  pageProps: {
+    session?: Session;
+    [key: string]: any;
+  };
+}
+
+export default function App({ Component, pageProps: { session, ...pageProps } }: MyAppProps) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
-  )
+  );
 }
+
